8dc523f24a7026d882d2e814f1b4cf067f0364a5
Gabriel Barreto <gh.barreto@hotmail.com>
2023-07-25 15:08:37 -0700

    added main

--- main.go
+++ main.go
@@ -1,4 +1,33 @@
 package main
 
+import (
+	"clima/key"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"os"
+)
+
 func main() {
+	data := fetchData()
+
+	fmt.Print(data)
+}
+
+func fetchData() string {
+	response, err := http.Get("https://api.openweathermap.org/data/2.5/forecast?q=Vancouver&appid=" + key.Env())
+
+	if err != nil {
+		fmt.Print(err.Error())
+		os.Exit(1)
+	}
+
+	data, err := ioutil.ReadAll(response.Body)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	return string(data)
 }
